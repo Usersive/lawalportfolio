1308056c89ce0d53cd2827cabff7a275e71a816b
Usersive <lawalusersive@gmail.com>
2025-02-13 14:51:14 +0100

    checkme

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -164,7 +164,7 @@ STATIC_ROOT=BASE_DIR /'staticfiles'
 
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR, "static")
-#     'myportfolio/static'
+    # 'myportfolio/static',
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
