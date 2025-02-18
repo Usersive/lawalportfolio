9b28fe735f101386792b9c3dffc50f9530c46a69
Usersive <lawalusersive@gmail.com>
2025-02-18 08:48:43 +0100

    workme

--- app/urls.py
+++ app/urls.py
@@ -4,9 +4,9 @@ from .views import file_list, download_file
 from django.conf.urls.static import static
 
 urlpatterns =[
-    path('', file_list, name='file_list'),
-    path('download/<int:file_id>/', download_file, name='download_file'),
+    # path('', file_list, name='file_list'),
+    # path('download/<int:file_id>/', download_file, name='download_file'),
 ]
-if settings.DEBUG:
-    urlpatterns +=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns +=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#     urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app/views.py
+++ app/views.py
@@ -1,21 +1,21 @@
-from django.shortcuts import get_object_or_404, render
-from django.http import FileResponse, Http404, HttpResponse, HttpResponseRedirect
-from .models import File
-import os
-from django.conf import settings
+# from django.shortcuts import get_object_or_404, render
+# from django.http import FileResponse, Http404, HttpResponse, HttpResponseRedirect
+# from .models import File
+# import os
+# from django.conf import settings
 
-def file_list(request):
-    files = File.objects.all()
-    return render(request, 'downloads/file_list.html', {'files': files})
+# def file_list(request):
+#     files = File.objects.all()
+#     return render(request, 'downloads/file_list.html', {'files': files})
 
-def download_file(request, file_id):
-    try:
-        file_obj = File.objects.get(id=file_id)
-        file_path = os.path.join(settings.MEDIA_ROOT, str(file_obj.file))
+# def download_file(request, file_id):
+#     try:
+#         file_obj = File.objects.get(id=file_id)
+#         file_path = os.path.join(settings.MEDIA_ROOT, str(file_obj.file))
 
-        return FileResponse(open(file_path, 'rb'), as_attachment=True)
-    except File.DoesNotExist:
-        raise Http404("File not found")
+#         return FileResponse(open(file_path, 'rb'), as_attachment=True)
+#     except File.DoesNotExist:
+#         raise Http404("File not found")
 
 
 # def download_file(request, file_id):
