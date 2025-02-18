95585c4f277f728a3f259aecc839cf40b0b28b49
Usersive <lawalusersive@gmail.com>
2025-02-18 07:54:43 +0100

    workflowme

--- app/views.py
+++ app/views.py
@@ -4,9 +4,9 @@ from .models import File
 import os
 from django.conf import settings
 
-def file_list(request):
-    files = File.objects.all()
-    return render(request, 'downloads/file_list.html', {'files': files})
+# def file_list(request):
+#     files = File.objects.all()
+#     return render(request, 'downloads/file_list.html', {'files': files})
 
 # def download_file(request, file_id):
 #     try:
@@ -18,15 +18,31 @@ def file_list(request):
 #         raise Http404("File not found")
 
 
+# def download_file(request, file_id):
+#     try:
+#         file_obj = get_object_or_404(File, id=file_id)
+
+#         # Cloudinary URL
+#         file_url = file_obj.file.url  # This should return the Cloudinary URL
+
+#         # Redirect to the Cloudinary URL for the file
+#         return HttpResponseRedirect(file_url)
+
+#     except File.DoesNotExist:
+#         raise Http404("File not found")
+
+import cloudinary
+import cloudinary.utils
+
 def download_file(request, file_id):
     try:
         file_obj = get_object_or_404(File, id=file_id)
+        
+        # Generate a signed URL for download
+        signed_url = cloudinary.utils.cloudinary_url(file_obj.file.name, secure=True, sign_url=True)
 
-        # Cloudinary URL
-        file_url = file_obj.file.url  # This should return the Cloudinary URL
-
-        # Redirect to the Cloudinary URL for the file
-        return HttpResponseRedirect(file_url)
+        # Redirect to the signed Cloudinary URL
+        return HttpResponseRedirect(signed_url[0])
 
     except File.DoesNotExist:
         raise Http404("File not found")
